19df9f4713b95c17f5d64803cc5beaf51beff9db
Julien <184573331+julien-bv@users.noreply.github.com>
2026-07-29 15:04:17 -0700

    Rewrite vehicle to be importable
    Restructure code in `Vehicle.py` to remove need to copy+paste code into each vehicle's code.

--- Vehicles/Vehicle.py
+++ Vehicles/Vehicle.py
@@ -128,8 +128,6 @@ class Vehicle:
         send_num += (self.vehicle_id << 6)
         radio.send_number(send_num)
 
-# Initialize vehicle object
-vehicle = Vehicle(..., ..., ...)
 
 # Constants
 
@@ -166,59 +164,3 @@ def decode_x(encoded_num: int):
 
 def decode_y(encoded_num: int):
     return (Y_MASK & encoded_num)
-
-# Access functions
-
-def is_valid_pos(vehicle_id: int):
-    return vehicle.positions[vehicle_id] >= 0
-
-def get_pos(vehicle_id: int):
-    return (get_x(vehicle_id), get_y(vehicle_id))
-
-def get_x(vehicle_id: int):
-    return decode_x(vehicle.positions[vehicle_id])
-
-def get_y(vehicle_id: int):
-    return decode_y(vehicle.positions[vehicle_id])
-
-# Radio functions 
-
-def on_received_number(received_number: int):
-    vehicle.interpret_information(received_number)
-
-# Input functions 
-
-def on_button_pressed_a():
-    if vehicle.scenario == 0:
-        vehicle.shift_x(1)
-        vehicle.show_current_position()
-    else:
-        maqueenPlusV2.control_motor_stop(maqueenPlusV2.MyEnumMotor.ALL_MOTOR)
-
-def on_button_pressed_b():
-    if vehicle.scenario == 0:
-        vehicle.shift_y(1)
-        vehicle.show_current_position() 
-    else:
-        pass
-        #maqueenPlusV2.control_motor(maqueenPlusV2.MyEnumMotor.ALL_MOTOR, maqueenPlusV2.MyEnumDir.FORWARD, NORMAL_SPEED) # speed not final
-
-# Forever loop
-
-def on_forever():
-    pass
-
-# Set-up 
-radio.on_received_number(on_received_number)
-
-input.on_button_pressed(Button.A, on_button_pressed_a)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-# Initialize motors
-maqueenPlusV2.i2c_init()
-
-# IMPLEMENT PATH FINDING ALGORITHM
-
-# IMPLEMENT CODE SPECIFIC TO EACH VEHICLE
-
-basic.forever(on_forever)
