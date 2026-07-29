de586cd3d48d1d044d95326a438cb29a27f5a785
Julien <184573331+julien-bv@users.noreply.github.com>
2026-07-29 13:51:45 -0700

    Update Vehicle and Traffic Controller code
    - Added functionality for transmitting and receiving action instructions to and from the traffic controller
    - Added global emergency stop to traffic controller
    - Removed unnecessary code

--- vehicle.py
+++ vehicle.py
@@ -30,20 +30,21 @@ class Vehicle:
 
         self.x_pos = 0 # Value between 0 and 4 (0b100) based on map above
         self.y_pos = 0 # Value between 0 and 4 (0b100) based on map above
-        self.show_stored_position()
+        self.show_current_position()
         self.transmit_position()
 
         self.scenario = 0
 
-    def update_channel(self, radio_channel: int):
-        radio.set_group(radio_channel)
+    def show_current_position(self):
+            basic.clear_screen()
+            led.plot(self.x_pos,self.y_pos)
 
     def set_x(self, x_new): # Set internal x_pos to new x_pos
         self.x_pos = x_new
         self.transmit_position()
                 
     def shift_x(self, dx):
-        self.x_pos += dx # Shift inter x_pos by dx
+        self.x_pos += dx # Shift internal x_pos by dx
         self.x_pos %= 5
         self.transmit_position()
         
@@ -52,9 +53,64 @@ class Vehicle:
         self.transmit_position()
         
     def shift_y(self, dy):
-        self.y_pos += dy # Shift inter x_pos by dx
+        self.y_pos += dy # Shift internal y_pos by dy
         self.y_pos %= 5
         self.transmit_position()
+
+    # Receive functions
+
+    def interpret_information(self, encoded_num:int):
+        self.store_position(encoded_num)
+
+        scenario = decode_scenario(encoded_num)
+        # If received scenario is a valid scenario, set the scenario
+        if 0 <= scenario < 0xe:  
+            self.scenario = scenario
+        else:
+            if scenario == 0xe or self.vehicle_id == decode_id(encoded_num): 
+                self.execute_action(decode_action(encoded_num))
+
+    def store_position(self, encoded_num: int):
+        self.positions[decode_id(encoded_num)] = decode_pos(encoded_num)
+
+    def execute_action(self, action:int):
+        if action == 0:
+            pass
+        elif action == 1:
+            pass
+        elif action == 2:
+            pass
+        elif action == 3:
+            pass
+        elif action == 4:
+            pass
+        elif action == 5:
+            pass
+        elif action == 6:
+            pass
+        elif action == 7:
+            pass
+        elif action == 8:
+            pass
+        elif action == 9:
+            pass
+        elif action == 10:
+            pass
+        elif action == 11:
+            pass
+        elif action == 12:
+            pass
+        elif action == 13:
+            pass
+        elif action == 14:
+            pass
+        elif action == 15:
+            maqueenPlusV2.control_motor_stop(maqueenPlusV2.MyEnumMotor.ALL_MOTOR)
+    
+    # Radio functions
+    
+    def update_channel(self, radio_channel: int):
+        radio.set_group(radio_channel)
 
     def transmit_position(self):
         # bits 7, 6 for vehicle id (4 possible IDs)
@@ -67,41 +123,15 @@ class Vehicle:
         send_num += self.y_pos
         radio.send_number(send_num)
 
-    def store_position(self, encoded_num: int):
-        self.positions[decode_id(encoded_num)] = decode_pos(encoded_num)
-
-    def show_stored_position(self):
-        basic.clear_screen()
-        led.plot(self.x_pos,self.y_pos)
-
-    def update_position_matrix(self): # adjust led brightness depending on how many cars are in each cell
-        basic.clear_screen()
-        for i in range(4):
-            if not is_valid_pos(i): 
-                led.plot(self.x_pos, self.y_pos)
-                # Skip execution of loop if there is not valid position data for vehicle with ID i
-                continue 
-
-            # Get the last known position of vehicle with ID i
-            x, y = get_pos(i) 
-            
-            # Increase the brightness of LED at position (x,y) by 63
-            # Max brightness of 252 when all four cars are in the same cell 
-            led.plot_brightness(x, y, led.point_brightness(x,y)+63) 
-
-    
+    def transmit_action(self, action:int): # Encodes an action between 0x00 and 0xff to be sent between vehicles 
+        send_num = action << 12
+        send_num += (self.vehicle_id << 6)
+        radio.send_number(send_num)
 
 # Initialize vehicle object
 vehicle = Vehicle(..., ..., ...)
 
 # Constants
-# Constant speeds depending on road signage, heavily subject to change
-SLOW_SPEED = 10 
-# Constant speeds depending on road signage, heavily subject to change
-NORMAL_SPEED = 100 
-# Constant speeds depending on road signage, heavily subject to change
-FAST_SPEED = 255 
-
 
 # bitwise AND-ing this masks and the number that encodes info about the
 # traffic situation then shifting the resulting number will
@@ -111,6 +141,7 @@ FAST_SPEED = 255
 # Next 2 bits are for vehicle ID
 # Next 3 bits are for X position
 # Next 3 bits are for Y position
+ACTION_MASK = 0b1111000000000000
 SCENARIO_MASK = 0b111100000000
 ID_MASK = 0b11000000
 POS_MASK = 0b111111
@@ -118,6 +149,8 @@ X_MASK = 0b111000
 Y_MASK = 0b111
 
 # Decode functions
+def decode_action(encoded_num: int):
+    return (ACTION_MASK & encoded_num) >> 12
 
 def decode_scenario(encoded_num: int):
     return (SCENARIO_MASK & encoded_num) >> 8
@@ -151,28 +184,31 @@ def get_y(vehicle_id: int):
 # Radio functions 
 
 def on_received_number(received_number: int):
-    vehicle.scenario = decode_scenario(received_number)
-    vehicle.store_position(received_number)
+    vehicle.interpret_information(received_number)
 
 # Input functions 
 
 def on_button_pressed_a():
     if vehicle.scenario == 0:
         vehicle.shift_x(1)
-        vehicle.show_stored_position()
+        vehicle.show_current_position()
     else:
         maqueenPlusV2.control_motor_stop(maqueenPlusV2.MyEnumMotor.ALL_MOTOR)
 
 def on_button_pressed_b():
     if vehicle.scenario == 0:
         vehicle.shift_y(1)
-        vehicle.show_stored_position() 
+        vehicle.show_current_position() 
     else:
-        maqueenPlusV2.control_motor(maqueenPlusV2.MyEnumMotor.ALL_MOTOR, maqueenPlusV2.MyEnumDir.FORWARD, NORMAL_SPEED) # speed not final
+        pass
+        #maqueenPlusV2.control_motor(maqueenPlusV2.MyEnumMotor.ALL_MOTOR, maqueenPlusV2.MyEnumDir.FORWARD, NORMAL_SPEED) # speed not final
+
+# Forever loop
+
+def on_forever():
+    pass
 
 # Set-up 
-led.set_display_mode(DisplayMode.GREYSCALE)
-
 radio.on_received_number(on_received_number)
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
@@ -181,10 +217,8 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 # Initialize motors
 maqueenPlusV2.i2c_init()
 
-# IMPLEMENT PATH FINDING ALGORITHMs
+# IMPLEMENT PATH FINDING ALGORITHM
 
 # IMPLEMENT CODE SPECIFIC TO EACH VEHICLE
 
-while True:
-    # IMPLEMENT LOGIC PER VEHICLE
-    break
+basic.forever(on_forever)
